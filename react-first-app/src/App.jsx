b428c52e8a207951b8bac61ab9275ccb2751f0c6
Lalit <lalitkumar68756@gmail.com>
2026-08-17 10:39:27 +0530

    print list

--- react-first-app/src/App.jsx
+++ react-first-app/src/App.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 
 export default function App() {
   let name = 'Lalit'
+  let nameList = ['Lalit', 'Rohit', 'Sahil', 'Ramesh'];
   return (
     <>
       <h1>Hello, <span className="text-red-500">{name.toLocaleUpperCase()}</span></h1>
+      <ul>
+        {nameList.map((n) => (
+          <li key={n}>{n.toUpperCase()}</li>
+        ))}
+      </ul>
     </>
   )
 }
