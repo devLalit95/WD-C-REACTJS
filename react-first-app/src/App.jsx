18e3a6b18aaab5c8fffb2150dac0562f29cbc722
Lalit <lalitkumar68756@gmail.com>
2026-08-19 11:50:07 +0530

    use state example

--- react-first-app/src/App.jsx
+++ react-first-app/src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { MyButton } from './components/MyButton'
+import { UseStateExample } from './components/UseStateExample';
 
 export default function App() {
   let name = 'Lalit'
   let nameList = ['Java', 'C++', 'React', 'ruby and rails', 'Python', 'JavaScript'];
   return (
     <>
-      {/* <h1>Hello, <span className="text-red-500">{name.toLocaleUpperCase()}</span></h1>
-      <ul>
-        {nameList.map((n) => (
-          <li key={n}>{n.toUpperCase()}</li>
-        ))}
-      </ul> */}
 
-      <MyButton name={name} />
+     <UseStateExample></UseStateExample>
     </>
   )
 }
